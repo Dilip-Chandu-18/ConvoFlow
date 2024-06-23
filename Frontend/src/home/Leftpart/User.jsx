9d776df5a7c8b86fc796836688fd0f820d443f87
dilip-chandu-18 <dilipchandu187@gmail.com>
2024-06-23 18:22:47 +0530

    first commit

---
+++ Frontend/src/home/Leftpart/User.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import useConversation from "../../zustand/useConversation.js";
+import { useSocketContext } from "../../context/SocketContext.jsx";
+
+function User({ user }) {
+  const { selectedConversation, setSelectedConversation } = useConversation();
+  const isSelected = selectedConversation?._id === user._id;
+  const { socket, onlineUsers } = useSocketContext();
+  const isOnline = onlineUsers.includes(user._id);
+  return (
+    <div
+      className={`hover:bg-slate-600 duration-300 ${
+        isSelected ? "bg-slate-700" : ""
+      }`}
+      onClick={() => setSelectedConversation(user)}
+    >
+      <div className="flex space-x-4 px-8 py-3 hover:bg-slate-700 duration-300 cursor-pointer">
+        <div className={`avatar ${isOnline ? "online" : ""}`}>
+          <div className="w-12 rounded-full">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpK2zMPUXYsZZgm5GAac9oa42cyxHtYGO5wA&s" />
+          </div>
+        </div>
+        <div>
+          <h1 className=" font-bold">{user.fullname}</h1>
+          <span>{user.email}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default User;
